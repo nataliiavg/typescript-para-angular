ce0c06f2d50f1c000180a820d9f76ad39963c791
Natalia Graciano <nataliavgraciano@gmail.com>
2023-09-29 16:18:43 -0300

    attribute decorator

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,40 @@
 // decorators
-function ExibirNome(target: any) {
-  console.log(target);
+// class decorator
+function apiVersion(version: string) {
+  return (target: any) => {
+    Object.assign(target.prototype, { ___version: version, __name: "natalia" });
+  };
 }
 
-@ExibirNome
-class Funcionario {}
+// attribute decorator
+function miniLength(length: number) {
+  return (target: any, key: string) => {
+    let _value = target[key];
 
-@ExibirNome
- class Quincas{}
+    const getter = () => "[play] " + _value;
+    const setter = (value: string) => {
+      if (value.length < length) {
+        throw new Error(`Tamanho menor do que ${length}`);
+      } else {
+        _value = value;
+      }
+    };
+
+    Object.defineProperty(target, key, {
+      get: getter,
+      set: setter,
+    });
+  };
+}
+
+class Api {
+  @miniLength(10)
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+const api = new Api("maiorque10");
+console.log(api.name);
